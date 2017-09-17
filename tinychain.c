1077214e673d5c0e6ce8150d49472e4ec67184a9
Tanner Hoelzel <thoelze1@binghamton.edu>
2017-09-17 02:41:40 -0400

    implement smart data structure

--- tinychain.c
+++ tinychain.c
@@ -3,7 +3,9 @@
  * @since 09/16/2017
  *
  * Links:
+ *      https://medium.freecodecamp.org/from-what-is-blockchain-to-building-a-blockchain-within-an-hour-4e738efc819d
  * 		https://stackoverflow.com/questions/14409466/simple-hash-functions
+ * 		http://ecomunsing.com/build-your-own-blockchain
  */
 
 #include <time.h>
@@ -15,23 +17,49 @@
 #include <sys/types.h>		// IN_ADDR
 #include <arpa/inet.h>		// host <-> network byte order
 
+#define TRADE_CAP 10
+
 /*
- * A block has a height, hash,
- * previous_hash, an amount of data, and a time.
+ * A trade has a payer, payee, and amount.
+ * 
+ * Payer 0 is the mint
+ */
+typedef struct trade {
+	unsigned int payer, payee;
+	int amt;
+	time_t time;
+} Trade;
+
+/*
+ * This is exactly the data that is hashed.
+ */
+typedef struct data {
+	unsigned int height, prev_hash, num_trades;
+	Trade trades[TRADE_CAP];
+	time_t time;
+} Data;
+
+/*
+ * A block has a parent block, a child block, data,
+ * and a hash.
  *
  * These members may have to change to accomodate
  * chain forking.
  */
 typedef struct block {
-	struct block *next, *prev;
-	unsigned int height, hash, prev_hash, data_size;
-	void *data;
-	time_t time;
+	struct block *prev, *next;
+	Data *data;
+	unsigned int hash;
 } Block;
 
 
 void print_block(Block *block) {
-	printf("Block %2d\n\tHash: %u\n\tTime: %lu\n", block->height, block->hash, block->time);
+	unsigned int counter;
+	printf("Block: %2d\n    Prev Hash: %u\n    Trades: %u\n", block->data->height, block->data->prev_hash, block->data->num_trades);
+	for(counter = 0; counter < block->data->num_trades; counter++) {
+			printf("        %.4d from %.4u to %.4u\n", block->data->trades[counter].amt, block->data->trades[counter].payer, block->data->trades[counter].payee);
+	}
+	printf("    Hash: %u\n\n", block->hash);
 }
 
 /*
@@ -39,9 +67,9 @@ void print_block(Block *block) {
  *
  * Returns hash as unsigned int
  */
-unsigned int tiny_hash(void *data, unsigned int data_size) {
+unsigned int tiny_hash(Data *data) {
 	unsigned int hash_address = 5831, counter;
-	for(counter = 0; counter < data_size; counter++) {
+	for(counter = 0; counter < sizeof(Data); counter++) {
 		hash_address = ((hash_address << 5) + hash_address) + ((char *)data)[counter];
 	}
 	return hash_address;
@@ -51,16 +79,20 @@ unsigned int tiny_hash(void *data, unsigned int data_size) {
  * Creates first block
  */
 Block *create_genesis_block() {
-	Block *genesis_block = (Block *) malloc (sizeof(Block));
+	// Genesis data
+	Data *genesis_data = (Data *)malloc(sizeof(Data));
+	genesis_data->height = 0;
+	genesis_data->prev_hash = 0;
+	genesis_data->num_trades = 2;
+	genesis_data->trades[0] = (Trade) {0,1,100,time(NULL)};
+	genesis_data->trades[1] = (Trade) {0,2,100,time(NULL)};
+	genesis_data->time = time(NULL);
+	// Genesis block;
+	Block *genesis_block = (Block *)malloc(sizeof(Block));
 	genesis_block->prev = NULL;
 	genesis_block->next = NULL;
-	genesis_block->height = 0;
-	genesis_block->data = malloc(13);
-	genesis_block->data_size = 13;
-	strncpy(genesis_block->data, "Genesis block", 13);
-	genesis_block->prev_hash = 0;
-	genesis_block->hash = tiny_hash(genesis_block->data, genesis_block->data_size);
-    genesis_block->time = time(NULL);
+	genesis_block->data = genesis_data;
+	genesis_block->hash = tiny_hash(genesis_block->data);
 	return genesis_block;
 }
 
@@ -68,17 +100,20 @@ Block *create_genesis_block() {
  * Creates new block
  */
 Block *create_next_block(Block *curr_block) {
-	Block *next_block = (Block *) malloc (sizeof(Block));
-	curr_block->next = next_block;
+    // Next data
+	Data *next_data = (Data *)malloc(sizeof(Data));
+	next_data->height = curr_block->data->height + 1;
+	next_data->prev_hash = curr_block->hash;
+	next_data->num_trades = 2; // this will change
+	next_data->time = time(NULL);
+	// Next block
+	Block *next_block = (Block *)malloc(sizeof(Block));
 	next_block->prev = curr_block;
 	next_block->next = NULL;
-	next_block->height = curr_block->height + 1;
-	next_block->data = malloc(sizeof(unsigned int));
-	*((unsigned int *)(next_block->data)) = (unsigned int)rand();
-	next_block->data_size = sizeof(unsigned int);
-	next_block->prev_hash = curr_block->hash;
-	next_block->hash = tiny_hash(next_block->data, next_block->data_size);
-    next_block->time = time(NULL);
+	next_block->data = next_data;
+	next_block->hash = tiny_hash(next_block->data);
+	// Prev block
+	curr_block->next = next_block;
 	return next_block;
 }
 
@@ -100,61 +135,6 @@ void build_chain(Block *genesis_block) {
 int main(int argc, char **argv) {
 	srand(time(NULL));
 	Block *genesis_block = create_genesis_block();
-	
-	// Server socket
-	int sockfd, connfd, n;
-	char send_buf[1024], recv_buf[1024];
-	struct sockaddr_in server_addr, dest_addr;
-	time_t ticks;
-
-	// "socket descriptor"
-	sockfd = socket(PF_INET, SOCK_STREAM, 0);	// starts at 3, increments
-	connfd = 0;
-
-	memset(send_buf, '0', sizeof(send_buf));
-	memset(recv_buf, '0', sizeof(recv_buf));
-
-	memset(&server_addr, '0', sizeof(server_addr));
-	server_addr.sin_family = PF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(5000);
-
-	// request that server_addr be bound to listen_fd, which is likely 3
-	bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-	// allow incoming connections, with 10 as queue limit
-	// if connection request arrives and queue is full, the connetion is refused
-	listen(sockfd, 10);
-
-	while(1) {
-		// extracts first connection request in the queue,
-		// copies socket with a new file descriptor
-		// (blocks until a connection is present)
-
-		connfd = accept(listenfd, (struct sockaddr *)NULL, NULL);
-
-		ticks = time(NULL);
-		snprintf(send_buf, sizeof(send_buf), "%.24s\r\n", ctime(&ticks));
-		write(connfd, send_buf, strlen(send_buf));
-
-		close(connfd);
-		sleep(1);
-	}
-
-    if(argc != 2) {
-		printf("usage: %s <server ip>\n", argv[0]);
-		exit(1);
-	}
-
-	// converts address in presentation format to network format 
-	inet_pton(PF_INET, argv[1], &serv_addr.sin_addr);
-
-	connect(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
-	while((n = read(sockfd, recv_buf, sizeof(recv_buf)-1)) > 0) {
-		recv_buf[n] = 0;
-		fputs(recv_buf, stdout);
-	}
-
+	build_chain(genesis_block);
 	return 0;
 }
